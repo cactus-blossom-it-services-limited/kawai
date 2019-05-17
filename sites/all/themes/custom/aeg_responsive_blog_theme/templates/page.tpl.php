9ebe9b14f212e59043354322e44132b863b3ce78
Andrew Farquharson <farquharson.a@gmail.com>
2019-05-17 15:09:50 +0100

    edit page.tpl.php, suppress errors

--- sites/all/themes/custom/aeg_responsive_blog_theme/templates/page.tpl.php
+++ sites/all/themes/custom/aeg_responsive_blog_theme/templates/page.tpl.php
@@ -127,7 +127,11 @@
   <?php print render($page['menu_bar']); ?>
 
   <!-- Messages and Help -->
-  <?php print $messages; ?>
+// Only display error messages if user logged in as admin
+    <?php if($is_admin): ?>
+        <?php print $messages; ?>
+    <?php endif; ?>
+
   <?php print render($page['help']); ?>
 
   <!-- region: Secondary Content -->
